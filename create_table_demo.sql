d91b4015ba03d66a3597cc6c3e5d63eb3ba694de
sairam482 <39360922+sairam482@users.noreply.github.com>
2026-08-13 17:34:12 +0530

    Fix schema name in SQL script

--- create_table_demo.sql
+++ create_table_demo.sql
@@ -3,7 +3,7 @@ create or replace database newgitdb;
 --set context 
 use warehouse instructor1_wh;
 use database newgitdb;
-use schema newgitdb.publicl
+use schema newgitdb.public;
 
 --create table and load data
 
